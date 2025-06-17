d3c2bea8b0615eb2c10a8d6dd050162521b53fcc
Anjali_Nanaware <nanawareanjali15@gmail.com>
2025-06-17 19:34:53 +0530

    Create LangingPage, AboutPage

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast'
+import LoginPage from './components/LoginPage'
+// import DashboardLayout from './components/Dashboard/DashboardLayout'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <NavBar/>
+      <Toaster position="bottom-centenpmr"/>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/about' element={<AboutPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        <Route path='/login' element={<LoginPage/>}/>
+        {/* <Route path='/dashboard' element={<DashboardLayout/>}/> */}
+
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/contextApi/ContextApi.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext } from "react";
+import { useState } from "react";
+
+export const ContextApi=createContext();
+
+export const ContextProvider=({children}) =>{
+    const getToken = localStorage.getItem("JWT_TOKEN")
+          ? JSON.parse(localStorage.getItem("JWT_TOKEN"))
+          :null
+    
+    const [token, setToken] =useState(getToken)
+
+    const sendData={
+        token,
+        setToken
+    }
+
+    return <ContextApi.Provider value={sendData}>{children}</ContextApi.Provider>
+};
+
+export const useStoreContext= ()=>{
+    const context=useContext(ContextApi);
+
+    return context;
+}
+
